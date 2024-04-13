e9047f7ec442eaacfcd9a153aeaf9b95fbeac253
nabby27 <ivancordoba77@gmail.com>
2024-04-13 14:35:19 +0200

    fix vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [],
     output: 'server',
+    adapter: vercel({
+    }),
 });
