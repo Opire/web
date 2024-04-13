f570c3d2328c53a81ff78d534e09e0fc14049a16
nabby27 <ivancordoba77@gmail.com>
2024-04-13 14:33:20 +0200

    fix umami analytics and remove vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [],
     output: 'server',
-    adapter: vercel({
-        webAnalytics: { enabled: true }
-    }),
 });
