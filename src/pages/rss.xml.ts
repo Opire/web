687f8a9ee54cdd9b8159cde1d4933bb816c15374
nabby27 <ivan.cordoba@ext.codely.com>
2024-09-03 16:28:34 +0200

    fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,4 +1,4 @@
-import rss from "@astrojs/rss";
+import rss, { type RSSFeedItem } from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context: any) {
@@ -7,10 +7,13 @@ export async function GET(context: any) {
         title: "Opire - the reward platform for software developers",
         description: "With Opire, anyone can create rewards for open source projects and grow their community, while developers can solve issues and earn the associated rewards.",
         site: context.site,
-        items: posts.map((post) => ({
-            ...post.data,
-            pubDate: new Date(post.data.pubDate),
-            link: `/blog/${post.slug}/`,
-        })),
+        items: posts.map((post) => {
+            return {
+                ...post.data,
+                pubDate: new Date(post.data.pubDate),
+                link: `/blog/${post.slug}/`,
+                author: post.data.author.name,
+            } as RSSFeedItem
+        }),
     });
 }
